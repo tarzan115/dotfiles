5233919d73f341d9eade9150eb797b4bd9f2d6d2
Doanh Van Luong <doanhlv@duck.com>
2026-03-01 18:22:10 -0500

    feat: add gcc, rust, cargo, binstall, kanata, caps

--- init.sh
+++ init.sh
@@ -1,10 +1,18 @@
+#! /bin/bash
+
 # installing tools
 sudo dnf copr enable scottames/ghostty atim/starship -y
 
-sudo dnf install ghostty nu starship -y
+sudo dnf install ghostty nu starship gcc -y
 
 ## zed
 curl -f https://zed.dev/install.sh | sh
+
+## rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+## kanata
+cargo install cargo-binstall kanata
 
 #
 
@@ -23,3 +31,29 @@ wget -P ~/.config https://raw.githubusercontent.com/thm-unix/thm-zshtheme/main/s
 
 ## config ghostty
 echo "command = /usr/bin/nu" >> ~/.config/ghostty/config
+
+# git config
+git config --global user.email "doanhlv@duck.com"
+git config --global user.name "Doanh Van Luong"
+
+# kanata config
+sudo groupdel uinput 2>/dev/null
+sudo groupadd --system uinput
+sudo usermod -aG input $USER
+sudo usermod -aG uinput $USER
+
+sudo tee /etc/udev/rules.d/99-input.rules > /dev/null <<EOF
+KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+EOF
+
+sudo udevadm control --reload-rules && sudo udevadm trigger
+
+mkdir -p ~/.config/kanata
+cp ./caps/kanata/config.kbd ~/.config/kanata/
+mkdir -p ~/.config/systemd/user
+cp ./caps/kanata/kanata.service ~/.config/systemd/user/
+
+systemctl --user daemon-reload
+systemctl --user enable kanata.service
+systemctl --user start kanata.service
+systemctl --user status kanata.service   # check whether the service is running
--- nushell/env.nu
+++ nushell/env.nu
@@ -1,3 +1,3 @@
 $env.config.buffer_editor = "zed"
 $env.config.show_banner = false
-# $env.config.color_config = false
+source $"($nu.home-path)/.cargo/env.nu"
